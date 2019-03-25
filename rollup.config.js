7805945f691af6445c0ce5452bdb3a44e4df746a
Prijesh <prijeshbhingradiya1@gmail.com>
2019-03-25 17:47:56 +0530

    Added babel rollup and react

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
+const dist = "dist";
+const bundle = "bundle";
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: "src/index.js",
+  external: ["react"],
+  output: [
+    {
+      file: `${dist}/${bundle}.cjs.js`,
+      format: "cjs"
+    },
+    {
+      file: `${dist}/${bundle}.esm.js`,
+      format: "esm"
+    },
+    {
+      name: "anything",
+      file: `${dist}/${bundle}.umd.js`,
+      format: "umd",
+      globals: {
+        react: "React"
+      }
+    }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "node_modules/**"
+    }),
+    production && terser()
+  ]
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hey");
+import React from "react";
+
+const Spinner = props => <h1>DVDSGS</h1>;
+
+export default Spinner;
